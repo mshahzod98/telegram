180cfd338846fe3397116c6115679c3a9d1b8eae
Shahzod Sh.Muhammadiev <148191240+mshahzod98@users.noreply.github.com>
2024-10-08 16:24:06 +0500

    Add files via upload

---
+++ telegram.php
@@ -0,0 +1,28 @@
+<?php
+
+/* https://api.telegram.org/bot7111484915:AAEgov0EpvB3cRrbWr-DayAyY2aUXoOLKS4/getUpdates,
+где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
+
+$name = $_POST['user_name'];
+$phone = $_POST['user_phone'];
+$email = $_POST['user_email'];
+$token = "7111484915:AAEgov0EpvB3cRrbWr-DayAyY2aUXoOLKS4";
+$chat_id = "-4167535736";
+$arr = array(
+  'Имя пользователя: ' => $name,
+  'Телефон: ' => $phone,
+  'Email' => $email
+);
+
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+if ($sendToTelegram) {
+  header('Location: thank-you.html');
+} else {
+  echo "Error";
+}
+?>
